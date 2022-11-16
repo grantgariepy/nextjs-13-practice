0b6314a917133352a640cd82cce2d6d4239a2382
grantgariepy <86595691+grantgariepy@users.noreply.github.com>
2022-11-16 11:01:56 -0700

    Update Coin.tsx

--- app/Coin.tsx
+++ app/Coin.tsx
@@ -1,9 +1,16 @@
+'use client'
+
 import Link from 'next/link'
 import React from 'react'
 
+let checked = false;
+const handleClick = () =>{
+  checked = !checked;
+}
+
 const Coin = ({name, id}:{name: string, id: string}) => {
   return (
-    <li key={id} >
+    <li key={id} onClick={handleClick} >
       <Link href={`/${id}`}>{name}</Link>
     </li>
   )
