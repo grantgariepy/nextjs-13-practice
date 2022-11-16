9cedd26e6f9f998a8160b03215a409b9f6b9d1a2
grantgariepy <86595691+grantgariepy@users.noreply.github.com>
2022-11-16 12:40:24 -0700

    Update Coin.tsx

--- app/Coin.tsx
+++ app/Coin.tsx
@@ -6,6 +6,7 @@ import React from 'react'
 let checked = false;
 const handleClick = () =>{
   checked = !checked;
+  console.log(checked);
 }
 
 const Coin = ({name, id}:{name: string, id: string}) => {
