7c1ab489c2337f0bf757fdd95b982a92b206d71b
grantgariepy <86595691+grantgariepy@users.noreply.github.com>
2022-11-18 09:17:57 -0700

    Update layout.tsx

--- app/[id]/layout.tsx
+++ app/[id]/layout.tsx
@@ -9,33 +9,9 @@ export default function RootLayout({
 }) 
 {
   return (
-    
-//     <body className=''>
-//               <div className='max-h-screen'>
-//           <div className='min-h-min' id='homePage'>
-//             <div className="drawer ">
-
-//               <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-//               <div className="drawer-content ">
-//               <Navbar />
-                <div>
-                  {children}
-                </div>
-//               </div>
-//               <div className="drawer-side">
-  //              {/* <!-- Sidebar content here --> */}
-//                 <label htmlFor="my-drawer" className="drawer-overlay overflow-y-hidden"></label>
-//                 <ul className="menu bg-base-100 w-56 p-2">
-                    {/* @ts-ignore */}
-//                     <Crypto />
- //                 {/* @ts-ignore */}
-   //               {/* <Search /> */}
-//                   </ul>
-//               </div> 
-//             </div>
-//           </div> 
-//               </div>
-//      </body> -->
+    <div>
+      {children}
+    </div>
     
   )
 }
